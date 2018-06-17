2c4a7ccb5261ce76aea42b5677085cfe75821491
tianchen <tczhong24@gmail.com>
2018-06-17 16:16:43 -0500

    ...

--- js/form.jsx
+++ js/form.jsx
@@ -16,24 +16,29 @@ class FlavorForm extends React.Component {
         fetch('https://us-central1-leetcode-207514.cloudfunctions.net/demo', {
             method: 'post',
             body: JSON.stringify({ "question": "1", "lang": this.state.value, "time": "10" })
-        }).then(response => response.json()).then(console.log(response))
+        }).then(function (response) {
+            return response.json();
+        }).then(function (response) {
+            console.log(response.code);
+        })
     };
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} >
-                <label>
-                    Pick your favorite language:
+};
+render() {
+    return (
+        <form onSubmit={this.handleSubmit} >
+            <label>
+                Pick your favorite language:
             <select value={this.state.value} onChange={this.handleChange}>
-                        <option value="java">java</option>
-                        <option value="python">python</option>
-                        <option value="c">c</option>
-                        <option value="cpp">cpp</option>
-                    </select>
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        );
-    }
+                    <option value="java">java</option>
+                    <option value="python">python</option>
+                    <option value="c">c</option>
+                    <option value="cpp">cpp</option>
+                </select>
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    );
+}
 }
 
 ReactDOM.render(
